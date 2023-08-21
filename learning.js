679a4f24e2e1ea2c832ed5764c0127735955971c
Douglas <douglimaonline@gmail.com>
2023-08-21 18:15:31 -0300

    Initial commit

---
+++ learning.js
@@ -0,0 +1,8 @@
+var totalBottles = 99;
+
+while(totalBottles > 0){
+    bottlesLeft = totalBottles - 1;
+    console.log (totalBottles + " bottles of beer on the wall, " + totalBottles + " bottles of beer.");
+    console.log ("Take one down and pass it around, " + bottlesLeft + " bottles of beer on the wall.");
+    totalBottles--;
+}
