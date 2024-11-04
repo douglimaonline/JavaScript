c347bb6d356ce173c23266035ad9c1a362cf135e
douglas <douglimaonline@gmail.com>
2024-11-04 18:34:37 -0300

    List exercise

--- Curso-nextjs/exercises/pages/basics/jsx.jsx
+++ Curso-nextjs/exercises/pages/basics/jsx.jsx
@@ -9,19 +9,47 @@ export default function knowingJSX() {
             </h1>
             <br />
             <h2>
-                {subtitle}
-            </h2>
-            <br />
-            <h2>
                 {currentDate}
             </h2>
-            <h4>
-                {between(7, 1, 9)}
-            </h4>
+            <br />
+            <div>
+                {listGenerate(15)}
+            </div>
+            <br />
+            <div>
+                {gerarLista(10)}
+            </div>
         </div>
     )
 };
 
-function between (value, min, max) {
-    if (value >= min && value <= max) return "Sim"
+// Exercise List
+
+function listGenerate (totalLines) {
+    let list = Array.from({length: totalLines}, (_, index) => index + 1)
+    let html = [];
+    list.forEach((number) => {
+        if (number === totalLines) {
+            html.push(<span key={number}>{number}</span>)
+        } else {
+            html.push(
+                <span key={number}>{number}, </span>
+            );
+        };
+    });
+    return html
+}
+
+// Solution
+
+function gerarLista (total) {
+    let list = [];
+    for (let i = 1; i <= total; i++) {
+        if (i === total) {
+            list.push(<span key={i}>{i}</span>)
+        } else {
+            list.push(<span key={i}>{i}, </span>)
+        };
+    };
+    return list
 }
