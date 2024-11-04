b78c174cfa8a610a4d92ec6a6e1735870d66eaf2
douglas <douglimaonline@gmail.com>
2024-11-04 18:46:08 -0300

    Fragment

---
+++ Curso-nextjs/exercises/pages/basics/fragment.jsx
@@ -0,0 +1,10 @@
+export default function fragment () {
+    return (
+        <> 
+        <h1>Title</h1>
+        <h2>Subtitle</h2>
+        </>
+    )
+}
+
+// The <> symbol, fix the fragment error on compilation.
