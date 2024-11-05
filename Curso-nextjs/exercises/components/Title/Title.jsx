b48423a9d881258196ff2861bcfd30207230f3e9
douglas <douglimaonline@gmail.com>
2024-11-05 01:02:17 -0300

    Style Module

--- Curso-nextjs/exercises/components/Title/Title.jsx
+++ Curso-nextjs/exercises/components/Title/Title.jsx
@@ -1,13 +1,15 @@
+import styles from "./style.module.css"
+
 export default function Title(props) {
     return props.small ?  (
-        <div className="title">
-        <h2 className="red">{props.main}</h2>
+        <div className={styles.title}>
+        <h2 className={styles.red}>{props.main}</h2>
         <p>{props.subtitle}</p>
         </div>
     ) : (
-        <div className="title">
+        <div className={styles.title}>
         <h1>{props.main}</h1>
-        <h2 className="blue">{props.subtitle}</h2>
+        <h2 className={styles.blue}>{props.subtitle}</h2>
         </div>
 
     )
--- Curso-nextjs/exercises/pages/_app.js
+++ Curso-nextjs/exercises/pages/_app.js
@@ -1,5 +1,4 @@
 import "@/styles/globals.css";
-import "@/components/Title/Title.css"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
