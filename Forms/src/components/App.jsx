587fc892cfd75a5aeaac1ca9c6fb121debc0cf74
douglas <douglimaonline@gmail.com>
2024-12-08 18:51:32 -0300

    React Forms

--- Forms/src/components/App.jsx
+++ Forms/src/components/App.jsx
@@ -1,27 +1,45 @@
 import React, { useState } from "react";
 
 function App() {
-  var [colorButton, setColorButton] = useState("white");
+  var [mouseOverButton, setMouseHoverButton] = useState(false);
+  var [name, setname] = useState("");
+  var [greetings, setGreetings] = useState("Hello");
 
   function overButton() {
-    setColorButton("black");
+    setMouseHoverButton(false);
   }
 
   function offButton() {
-    setColorButton("white");
+    setMouseHoverButton(true);
+  }
+
+  function onChange() {
+    setname(event.target.value);
+  }
+
+  function onClick(event) {
+    setGreetings(name === "" ? "Hello" : `Hello, ${name}`);
+    event.preventDefault();
   }
 
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button
-        style={{ backgroundColor: colorButton }}
-        onMouseOver={overButton}
-        onMouseOut={offButton}
-      >
-        Submit
-      </button>
+      <h1>{greetings}</h1>
+      <form onSubmit={onClick}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="What's your name?"
+        />
+        <button
+          type="submit"
+          style={{ backgroundColor: mouseOverButton ? "white" : "black" }}
+          onMouseOver={overButton}
+          onMouseOut={offButton}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
