f818294c333d01b1590413a8a467e1012ac4771a
douglas <douglimaonline@gmail.com>
2025-01-04 17:26:17 -0300

    Add module.css on Chess Board Exercise

--- Curso-nextjs/exercises/components/Chess/square.jsx
+++ Curso-nextjs/exercises/components/Chess/square.jsx
@@ -1,10 +1,11 @@
+import styles from './square.module.css'
+
 export default function Square(prop) {
   return (
     <div
+      className={styles.square}
       style={{
         background: prop.white ? 'white' : 'gray',
-        width: '70px',
-        height: '70px',
       }}
     ></div>
   )
