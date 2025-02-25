ca7323c1f90f9d5c81c65409605a913caf3c1401
douglas <douglimaonline@gmail.com>
2025-02-25 19:55:44 -0300

    Example of static page (Server-Side Rendering)

--- Curso-nextjs/renderization/pages/static.jsx
+++ Curso-nextjs/renderization/pages/static.jsx
@@ -1,3 +1,21 @@
+// import { useEffect, useState } from 'react'
+
 export default function Static() {
-  return <h1>Static</h1>
+  // const [num, setNum] = useState()
+
+  // useEffect(() => {
+  //   setNum(Math.random())
+  // }, [])
+
+  return (
+    <div>
+      <h1>Static #1</h1>
+      {/* <h1>{num}</h1> */}
+    </div>
+  )
 }
+
+// Explanation:
+// The Next automatically use the better strategy of renderizarion according to your code.
+// On the exemple above, the page will be render on server side.
+// If I take off the comments, the render will change from SSR (Server-Side Rendering) to CSR (Client-Side Rendering).
