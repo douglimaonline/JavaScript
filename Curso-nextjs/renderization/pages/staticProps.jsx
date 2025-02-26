46180f21ef84e03f960baef9054abda925bae87b
douglas <douglimaonline@gmail.com>
2025-02-25 22:04:18 -0300

    Example of Static Props

---
+++ Curso-nextjs/renderization/pages/staticProps.jsx
@@ -0,0 +1,20 @@
+export function getStaticProps() {
+  return {
+    revalidate: 6,
+    props: {
+      number: Math.random(),
+    },
+  }
+}
+
+export default function Static(props) {
+  return (
+    <>
+      <h1>static Props</h1>
+      <span>{props.number}</span>
+    </>
+  )
+}
+
+// Explanation:
+// Using getStaticProps function force the next to generate only once the data you are getting.
